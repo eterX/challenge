d309041636a5bcce453a7c5a7a6c00ad08b436ad
eterX <leandro.batlle@gmail.com>
2017-10-08 15:00:08 -0300

    GoToGoaland Proportional controller. New action GoToGoal (switched from polar to cartesian). New proportional controller (from https://github.com/clebercoutof/turtlesim_cleaner)

--- catkin_ws/src/ged/scripts/ged_server.py
+++ catkin_ws/src/ged/scripts/ged_server.py
@@ -6,7 +6,7 @@
 #
 #
 #std
-import sys,math,time
+import sys,math as m,time
 
 #ROS
 import rospy
@@ -48,7 +48,9 @@ trails_collection=list()
 cursor_move_template="cursor_move({id},{x},{y},{show});"
 cursor_rotate_template="cursor_rotate({id},{theta});"
 trail_points_template="trail_points({id},{points});"
-
+distance_tolerance=0.2
+pose_callback_period=0.1 #[secs]
+rospy_Rate=15
 
 class cursor(object): #python2
     """
@@ -74,7 +76,7 @@ class cursor(object): #python2
                                                   x=str(int(x*1000/11)), #TODO: send this to a "representation layer"
                                                   y=str(int(1000*(1-y/11))),
                                                   show=show))
-        result.append(cursor_rotate_template.format(id=str(self.id),theta=int(-theta*360/2/math.pi)))
+        result.append(cursor_rotate_template.format(id=str(self.id),theta=int(-theta*360/2/m.pi)))
         result.append(trail_points_template.format(id=str(self.id),points=self.trail.points))
 
 
@@ -116,19 +118,22 @@ class gedAction(object):
     
     #_feedback = gmsg.goToPointPolarActionFeedback()
     #_result = gmsg.goToPointPolarActionResult() #has header and goalid
-    _feedback = gmsg.goToPointPolarFeedback()
-    _result = gmsg.goToPointPolarResult()
+
+    #switched from polar to cartesian
+    #_feedback = gmsg.goToPointPolarFeedback()
+    #_result = gmsg.goToPointPolarResult()
+    _feedback = gmsg.goToPointFeedback()
+    _result = gmsg.goToPointResult()
 
     def __init__(self, name):
         self._action_name = name
+
         self.pose = Pose()
-        #self._as = actionlib.SimpleActionServer(self._action_name, actionlib_tutorials.msg.FibonacciAction, execute_cb=self.execute_cb, auto_start = False)
-        self._as = actionlib.SimpleActionServer(self._action_name, gmsg.goToPointPolarAction, execute_cb=self.execute_cb,  auto_start = False)
-        self._as.start() #do not replace with auto_start...
-
-
+        self.pose_callback_last = 0
         self.pubtwist = rospy.Publisher('/ged/turtle1/cmd_vel', geomsg.Twist, queue_size=10)
         self.pubWeb= rospy.Publisher('/ged/listener', smsg.String, queue_size=10)
+        self._as = actionlib.SimpleActionServer(self._action_name, gmsg.goToPointAction, execute_cb=self.execute_cb,  auto_start = False)
+        self._as.start() #do not replace with auto_start...
 
         for i in range(trails_collection_size):
             mitrail=trail(id=i)
@@ -161,29 +166,47 @@ class gedAction(object):
             self.pubWeb.publish(pubWebMsg)
 
 
+    def get_distance(self, goal_x, goal_y):
+        """
+        Proportional Controller (thanks to https://github.com/clebercoutof/turtlesim_cleaner)
+
+        :param goal_x:
+        :param goal_y:
+        :return: float # distance from current pos to goal
+        """
+        distance = m.sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
+        return distance
+
 
 
     def pose_callback(self, data):
-        print("pose_callback")
-        self.pose = data
-        self.pose.x = round(self.pose.x, 4)
-        self.pose.y = round(self.pose.y, 4)
-        self._feedback.xfeed=self.pose.x
-        self._feedback.yfeed=self.pose.y
-        self._as.publish_feedback(self._feedback) #keep the client in the loop
-        if ( monitor_updates ):
-            self.monitor_update(1) #TODO: handle more cursors
+        #global read-only pose_callback_period
+        #cadence of pose msgs processed,
+        #TODO: get a ROS-native way for throttling pose updates
+        if (self.pose_callback_last +  pose_callback_period  < time.time()):
+            print("pose_callback tstamp: {}".format(str(time.time())))
 
+            self.pose = data
+            self.pose.x = round(self.pose.x, 4)
+            self.pose.y = round(self.pose.y, 4)
+            if self._as.current_goal is not None:
+                self._feedback.xfeed=self.pose.x
+                self._feedback.yfeed=self.pose.y
+                self._as.publish_feedback(self._feedback) #keep the client in the loop
+            if ( monitor_updates ):
+                self.monitor_update(1) #TODO: handle cursors_collection
+            self.pose_callback_last=time.time()
 
 
     def execute_cb(self, goal):
         # helper variables
-        r = rospy.Rate(5)
+        r = rospy.Rate(rospy_Rate)
         success = True
         #Callback function implementing the pose value received
 
         # publish info to the console for the user
         rospy.loginfo('%s: Executing, ' % (self._action_name))
+        self.pose_subscriber = rospy.Subscriber('/ged/turtle1/pose', Pose, self.pose_callback)
 
 
         # start executing the action
@@ -196,30 +219,42 @@ class gedAction(object):
             #self._feedback.sequence = actionlib.GoalStatus.PREEMPTED
         else:
             #miwist=geomsg.Twist(linear=geomsg.Vector3(x=goal.len),angular=geomsg.Vector3(x=goal.ang))
-            if goal.ang != 0:
-                miwist=geomsg.Twist(angular=geomsg.Vector3(z=-goal.ang*2*math.pi/360)) #to rad CW
-                self.pubtwist.publish(miwist,)
 
-            if goal.len != 0:
-                miwist=geomsg.Twist(linear=geomsg.Vector3(x=goal.len))
-                self.pose_subscriber = rospy.Subscriber('/ged/turtle1/pose', Pose, self.pose_callback)
+            #switched from polar to cartesian
+            #if goal.ang != 0:
+            #    miwist=geomsg.Twist(angular=geomsg.Vector3(z=-goal.ang*2*m.pi/360)) #to rad CW
+            #    self.pubtwist.publish(miwist,)
+            #if goal.len != 0:
+            miwist=geomsg.Twist()
+            while m.sqrt(pow((goal.x - self.pose.x), 2) + pow((goal.y - self.pose.y), 2)) >= distance_tolerance:
+
+                #Proportional Controller (thanks to https://github.com/clebercoutof/turtlesim_cleaner)
+                #linear velocity in the x-axis:
+                miwist.linear.x = 1.5 * m.sqrt(pow((goal.x - self.pose.x), 2) + pow((goal.y - self.pose.y), 2))
+                miwist.linear.y = 0
+                miwist.linear.z = 0
+
+                #angular velocity in the z-axis:
+                miwist.angular.x = 0
+                miwist.angular.y = 0
+                miwist.angular.z = 4 * (m.atan2(goal.y - self.pose.y, goal.x - self.pose.x) - self.pose.theta)
+
+                #Publishing our vel_msg
                 self.pubtwist.publish(miwist)
                 r.sleep()
-                self.pose_subscriber.unregister()
-                #                self.pubtwist.publish(miwist)
-                #                    self._as.publish_feedback(self._feedback)
-                #                    r.sleep()
-
-
-                #self._feedback.status = actionlib.GoalStatus.SUCCEEDED #no need, state is a member or result
-                # this step is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-                #        r.sleep()
+                #self.pose_subscriber.unregister()
+            #self.pubtwist.publish(miwist)
+            #r.sleep()
+            miwist.linear.x = 0
+            miwist.angular.z =0
+            self.pubtwist.publish(miwist)
+            self.pose_subscriber.unregister()
 
         if success:
             #self._result.result(x=1,y=2)
             #self._result.sequence = self._feedback.sequence
             rospy.loginfo('%s: Succeeded' % self._action_name)
-            #self._as.set_succeeded(self._result)
+            self._as.set_succeeded(self._result)
 
 if __name__ == '__main__':
     rospy.init_node('ged_server_py',)
