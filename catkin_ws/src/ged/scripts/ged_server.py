13a4135840519f694d214707bd005fb18176cc3b
eterX <leandro.batlle@gmail.com>
2017-10-09 01:39:40 -0300

    can't find the problem with the P controller, teleporting

--- catkin_ws/src/ged/scripts/ged_server.py
+++ catkin_ws/src/ged/scripts/ged_server.py
@@ -6,11 +6,12 @@
 #
 #
 #std
-import sys,math as m,time
+import sys,math,time
 
 #ROS
 import rospy
 from turtlesim.msg import Pose
+from turtlesim.srv import *
 
 #Ged
 import ACBged as acbg #Ged's own package, svr and cnt stuff
@@ -48,9 +49,9 @@ trails_collection=list()
 cursor_move_template="cursor_move({id},{x},{y},{show});"
 cursor_rotate_template="cursor_rotate({id},{theta});"
 trail_points_template="trail_points({id},{points});"
-distance_tolerance=1
+distance_tolerance=0.5
 pose_callback_period=0.1 #[secs]
-rospy_Rate=10
+rospy_Rate=1
 
 class cursor(object): #python2
     """
@@ -76,7 +77,7 @@ class cursor(object): #python2
                                                   x=str(int(x*1000/11)), #TODO: send this to a "representation layer"
                                                   y=str(int(1000*(1-y/11))),
                                                   show=show))
-        result.append(cursor_rotate_template.format(id=str(self.id),theta=int(-theta*360/2/m.pi)))
+        result.append(cursor_rotate_template.format(id=str(self.id),theta=int(-theta*360/2/math.pi)))
         result.append(trail_points_template.format(id=str(self.id),points=self.trail.points))
 
 
@@ -113,9 +114,6 @@ class trail(object): #python2
 
 class gedAction(object):
     # create messages that are used to publish feedback/result
-    # _feedback = actionlib_tutorials.msg.FibonacciFeedback()
-    # _result = actionlib_tutorials.msg.FibonacciResult()
-    
     #_feedback = gmsg.goToPointPolarActionFeedback()
     #_result = gmsg.goToPointPolarActionResult() #has header and goalid
 
@@ -124,7 +122,6 @@ class gedAction(object):
     #_result = gmsg.goToPointPolarResult()
     _feedback = gmsg.goToPointFeedback()
     _result = gmsg.goToPointResult()
-
     def __init__(self, name):
         self._action_name = name
 
@@ -132,8 +129,9 @@ class gedAction(object):
         self.pose_callback_last = 0
         self.pubtwist = rospy.Publisher('/ged/turtle1/cmd_vel', geomsg.Twist, queue_size=10)
         self.pubWeb= rospy.Publisher('/ged/listener', smsg.String, queue_size=10)
-        self._as = actionlib.SimpleActionServer(self._action_name, gmsg.goToPointAction, execute_cb=self.execute_cb,  auto_start = False)
-        self._as.start() #do not replace with auto_start...
+        self.pose_subscriber = rospy.Subscriber('/ged/turtle1/pose', Pose, self.pose_callback)
+        self.r = rospy.Rate(rospy_Rate)
+
 
         for i in range(trails_collection_size):
             mitrail=trail(id=i)
@@ -145,6 +143,9 @@ class gedAction(object):
 
         cursors_collection[0].show=False #for tests
         cursors_collection[1].show=True #default cursor
+
+        self._as = actionlib.SimpleActionServer(self._action_name, gmsg.goToPointAction, execute_cb=self.execute_cb,  auto_start = False)
+        self._as.start() #do not replace with auto_start...
 
 
     def monitor_update(self,id):
@@ -174,7 +175,7 @@ class gedAction(object):
         :param goal_y:
         :return: float # distance from current pos to goal
         """
-        distance = m.sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
+        distance = math.sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
         return distance
 
 
@@ -188,25 +189,23 @@ class gedAction(object):
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
 #            if self._as.current_goal is not None:
-        if (self.pose_callback_last +  pose_callback_period  < time.time()):
-            #print("pose_callback tstamp: {}".format(str(time.time())))
-            self._feedback.xfeed=self.pose.x
-            self._feedback.yfeed=self.pose.y
-            self._as.publish_feedback(self._feedback) #keep the client in the loop
-            if ( monitor_updates ):
-                self.monitor_update(1) #TODO: handle cursors_collection
-            self.pose_callback_last=time.time()
+#         if (self.pose_callback_last +  pose_callback_period  < time.time()):
+#             #print("pose_callback tstamp: {}".format(str(time.time())))
+#             self._feedback.xfeed=self.pose.x
+#             self._feedback.yfeed=self.pose.y
+#             self._as.publish_feedback(self._feedback) #keep the client in the loop
+#             if ( monitor_updates ):
+#                 self.monitor_update(1) #TODO: handle cursors_collection
+#             self.pose_callback_last=time.time()
 
 
     def execute_cb(self, goal):
         # helper variables
-        r = rospy.Rate(rospy_Rate)
         success = True
         #Callback function implementing the pose value received
 
         # publish info to the console for the user
         rospy.loginfo('%s: Executing, ' % (self._action_name))
-        self.pose_subscriber = rospy.Subscriber('/ged/turtle1/pose', Pose, self.pose_callback)
 
 
         # start executing the action
@@ -218,39 +217,54 @@ class gedAction(object):
             #self._feedback.sequence = ["Preempted",]
             #self._feedback.sequence = actionlib.GoalStatus.PREEMPTED
         else:
-            #miwist=geomsg.Twist(linear=geomsg.Vector3(x=goal.len),angular=geomsg.Vector3(x=goal.ang))
+            #mitwist=geomsg.Twist(linear=geomsg.Vector3(x=goal.len),angular=geomsg.Vector3(x=goal.ang))
 
             #switched from polar to cartesian
             #if goal.ang != 0:
-            #    miwist=geomsg.Twist(angular=geomsg.Vector3(z=-goal.ang*2*m.pi/360)) #to rad CW
-            #    self.pubtwist.publish(miwist,)
+            #    mitwist=geomsg.Twist(angular=geomsg.Vector3(z=-goal.ang*2*math.pi/360)) #to rad CW
+            #    self.pubtwist.publish(mitwist,)
             #if goal.len != 0:
-            miwist=geomsg.Twist()
-#            while m.sqrt(pow((goal.x - self.pose.x), 2) + pow((goal.y - self.pose.y), 2)) >= distance_tolerance:
-            while self.get_distance(goal.x,goal.y) > distance_tolerance:
-                print(goal.x,goal.y, self.pose.x, self.pose.y, distance_tolerance, self.get_distance(goal.x,goal.y))
+            mitwist=geomsg.Twist()
+            i_msg=0
+            i_msg_max=10
+            while (self.get_distance(goal.x,goal.y) > distance_tolerance) and (i_msg < i_msg_max):
+                i_msg+=1
+                #print(goal.x,goal.y, self.pose.x, self.pose.y, distance_tolerance, self.get_distance(goal.x,goal.y))
 
                 #Proportional Controller (thanks to https://github.com/clebercoutof/turtlesim_cleaner)
                 #linear velocity in the x-axis:
-                miwist.linear.x = 1.5 * m.sqrt(pow((goal.x - self.pose.x), 2) + pow((goal.y - self.pose.y), 2))
-                miwist.linear.y = 0
-                miwist.linear.z = 0
+                mitwist.linear.x = 1.5 * math.sqrt(pow((goal.x - self.pose.x), 2) + pow((goal.y - self.pose.y), 2))
+                mitwist.linear.y = 0
+                mitwist.linear.z = 0
 
                 #angular velocity in the z-axis:
-                miwist.angular.x = 0
-                miwist.angular.y = 0
-                miwist.angular.z = 4 * (m.atan2(goal.y - self.pose.y, goal.x - self.pose.x) - self.pose.theta)
+                mitwist.angular.x = 0
+                mitwist.angular.y = 0
+                mitwist.angular.z = 4 * (math.atan2(goal.y - self.pose.y, goal.x - self.pose.x) - self.pose.theta)
 
                 #Publishing our vel_msg
-                self.pubtwist.publish(miwist)
-                r.sleep()
+                self.pubtwist.publish(mitwist)
+                self.r.sleep()
                 #self.pose_subscriber.unregister()
-            #self.pubtwist.publish(miwist)
+            if (self.get_distance(goal.x,goal.y) > distance_tolerance):
+                # looks like there is a problem that makes P controller go into infinite loops, until it's fixed, teleporting
+                rospy.wait_for_service('/ged/turtle1/teleport_absolute')
+                try:
+                    teleport = rospy.ServiceProxy('/ged/turtle1/teleport_absolute', TeleportAbsolute)
+                    response = teleport(goal.x,goal.y,0)
+                    print("Teleport response: {}".format(response))
+                    rospy.logwarn("Teleported: {}".format(response))
+                except rospy.ServiceException, e:
+                    print "Service call failed: %s"%e
+
+
+
+            #self.pubtwist.publish(mitwist)
             #r.sleep()
-            #miwist.linear.x = 0
-            #miwist.angular.z =0
-            #self.pubtwist.publish(miwist)
-            self.pose_subscriber.unregister()
+            #mitwist.linear.x = 0
+            #mitwist.angular.z =0
+            #self.pubtwist.publish(mitwist)
+        rospy.spin()
 
         if success:
             #self._result.result(x=1,y=2)
